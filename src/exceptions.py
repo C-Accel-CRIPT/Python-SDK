ed5c3f48d5558d8b918aca44f7df397431dfeb71
nh916 <navidhariri25@gmail.com>
2023-03-06 15:55:56 -0800

    fixed grammar of the ConnectionError exception

--- src/exceptions.py
+++ src/exceptions.py
@@ -1,10 +1,10 @@
 class ConnectionError(Exception):
     """
-    Raised when API object cannot connect to CRIPT with the give host and token
+    Raised when the API object cannot connect to CRIPT with the given host and token
     """
 
     def __init__(self):
         pass
 
     def __str__(self):
-        return "Could not connect to CRIPT with the given host and token. Please be sure both host and token are correct."
+        return "Could not connect to CRIPT with the given host and token. Please be sure both host and token are entered correctly."

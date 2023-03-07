d22c56ecc717bf16d3e32e692148ea8d7640e199
nh916 <navidhariri25@gmail.com>
2023-03-06 17:53:11 -0800

    setting up tests for getting db schema

--- tests/api/test_api.py
+++ tests/api/test_api.py
@@ -7,10 +7,14 @@ def cript_api():
     """
     create api for the rest of the tests to use
     """
-    host = ""
-    token = ""
+    host = "http://development.api.criptapp.org/"
+    token = "123456"
 
     return cript.API(host, token)
+
+
+def test_get_db_schema(cript_api):
+    cript_api._get_db_schema()
 
 
 def search_material_by_url(cript_api):

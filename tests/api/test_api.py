11a3c6e664f7a06a660ed64e83f43a27f256d24a
nh916 <navidhariri25@gmail.com>
2023-03-08 15:26:35 -0800

    added tests for api

--- tests/api/test_api.py
+++ tests/api/test_api.py
@@ -3,19 +3,90 @@ import pytest
 
 
 @pytest.fixture(scope="session")
-def cript_api():
+def cript_api() -> cript.API:
     """
-    create api for the rest of the tests to use
+    Create an API instance for the rest of the tests to use.
+
+    Returns:
+        API: The created API instance.
     """
     host = "http://development.api.criptapp.org/"
     token = "123456"
-
     return cript.API(host, token)
 
 
 def test_get_db_schema(cript_api):
+    """
+    just checks if that function gives anything back or not.
+    Parameters
+    ----------
+    cript_api
+
+    Returns
+    -------
+    NoneType
+        None
+
+    """
     cript_api._get_db_schema()
 
 
-def search_material_by_url(cript_api):
+def test_api_save_material(cript_api):
+    """
+    Tests if API object can successfully save a node
+    Parameters
+    ----------
+    cript_api
+
+    Returns
+    -------
+    None
+    """
     pass
+
+
+def test_api_search_material_by_url(cript_api):
+    """
+    Tests if the api can get the node it saved previously from the backend.
+    Tests search function directly, and indirectly tests if the material
+    that was already saved is actually saved and can be gotten
+
+    Parameters
+    ----------
+    cript_api
+
+    Returns
+    -------
+    None
+    """
+    pass
+
+
+def test_api_update_material(cript_api):
+    """
+    Tests if the API can get a material and then update it and save it in the database,
+    and after save it gets the material again and checks if the update was done successfully.
+
+    Parameters
+    ----------
+    cript_api
+
+    Returns
+    -------
+
+    """
+
+
+def test_api_delete_material(cript_api):
+    """
+    Tests if API can successfully delete a material.
+    After deleting it from the backend, it tries to get it, and it should not be able to
+
+    Parameters
+    ----------
+    cript_api
+
+    Returns
+    -------
+
+    """

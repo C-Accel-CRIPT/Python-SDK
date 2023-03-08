fe23928c2018a8c3c0ea2fcf96f2a3024ddda320
nh916 <navidhariri25@gmail.com>
2023-03-08 15:49:05 -0800

    created primary_node.py

---
+++ src/nodes/primary_node.py
@@ -0,0 +1,9 @@
+from abc import ABC
+
+
+class PrimaryNode(ABC):
+    """
+    Abstract class that defines what it means to be a PrimaryNode,
+    and other primary nodes can inherit from.
+    """
+    pass

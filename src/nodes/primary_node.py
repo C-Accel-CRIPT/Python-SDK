625780f5bdb7715c62628c07abc532f70a5dd9df
nh916 <navidhariri25@gmail.com>
2023-03-08 16:20:30 -0800

    working on the abstract primary_node.py

--- src/nodes/primary_node.py
+++ src/nodes/primary_node.py
@@ -1,4 +1,6 @@
 from abc import ABC
+from dataclasses import dataclass, field
+from typing import Union
 
 
 class PrimaryNode(ABC):
@@ -7,9 +9,17 @@ class PrimaryNode(ABC):
     and other primary nodes can inherit from.
     """
 
+    @dataclass(frozen=True)
+    class NodeAttributes:
+        url: Union[str, None] = None
+        uid: Union[str, None] = None
+        locked: bool = False
+
     def __str__(self) -> str:
         """
         Return a string representation of a primary node.
+        Every node that inherits from this class should overwrite it to best fit
+        their use case, but this provides a nice default value just in case
 
         Returns
         -------

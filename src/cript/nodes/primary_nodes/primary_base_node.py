01d4055168747bea38ad64f2521518bccc9e8f62
nh916 <navidhariri25@gmail.com>
2023-03-09 18:20:16 -0800

    added all fields and default values to primary_base_node.py

--- src/cript/nodes/primary_nodes/primary_base_node.py
+++ src/cript/nodes/primary_nodes/primary_base_node.py
@@ -1,5 +1,37 @@
 from abc import ABC
+from dataclasses import dataclass
+from typing import Union
+from src.nodes.supporting_nodes.user import User
 
 
 class PrimaryBaseNode(ABC):
-    pass
+    """
+    Abstract class that defines what it means to be a PrimaryNode,
+    and other primary nodes can inherit from.
+    """
+
+    @dataclass(frozen=True)
+    class NodeAttributes:
+        """
+        All shared attributes between all Primary nodes and set to their default values
+        """
+        url: Union[str, None] = None
+        uid: Union[str, None] = None
+        locked: bool = False
+        model_version: str = ""
+        updated_by: User = None
+        created_by: User = None
+        public: bool = False
+        name: str = ""
+        notes: str = ""
+
+    def __str__(self) -> str:
+        """
+        Return a string representation of a primary node.
+        Every node that inherits from this class should overwrite it to best fit
+        their use case, but this provides a nice default value just in case
+
+        Returns
+        -------
+        str: A string representation of a primary node.
+        """

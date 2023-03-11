f18b3c99fb7bf55a1da222e37b52d47e0779e59e
Ludwig Schneider <ludwigschneider@uchicago.edu>
2023-03-10 18:57:38 -0600

    add missing file

---
+++ src/cript/nodes/core.py
@@ -0,0 +1,27 @@
+from abc import ABC
+from dataclasses import dataclass, asdict, replace
+
+class BaseNode(ABC):
+    """
+    This abstract class is the base of all CRIPT nodes.
+    It offers access to a json attribute class, which reflects the data model JSON attributes.
+    Also, some basic shared functionality is provided by this base class.
+    """
+    @dataclass(frozen=True)
+    class JsonAttributes:
+        node: str = ""
+
+    _json_attrs: JsonAttributes = JsonAttributes()
+    def __init__(self, node):
+        self._json_attrs = replace(self._json_attrs, node=node)
+
+    def __str__(self) -> str:
+        """
+        Return a string representation of a node data model attributes.
+
+        Returns
+        -------
+        str
+            A string representation of the node.
+        """
+        return str(asdict(self._json_attrs))

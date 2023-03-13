8df5993c02d923cdf47ee758384e19952f46b8a1
Ludwig Schneider <ludwigschneider@uchicago.edu>
2023-03-13 10:52:44 -0500

    Make Cript installable via `pip` (#16)
    * make cript installable
    
    * add install test line
    
    * explicitly request python3 in tests
    
    * add missing nodes
    
    * fixing init files
    
    * add missing file
    
    * simplify test
    
    * fix imports

---
+++ src/cript/nodes/__init__.py
@@ -0,0 +1,30 @@
+from cript.nodes.primary_nodes import (
+    Collection,
+    ComputationalProcess,
+    Computation,
+    Data,
+    Experiment,
+    Inventory,
+    Process,
+    Project,
+    Reference,
+    Software,
+)
+
+from cript.nodes.subobjects import (
+    Algorithm,
+    Citation,
+    Equipment,
+    Identifier,
+    Ingredient,
+    Parameter,
+    Property,
+    Quantity,
+    SoftwareConfiguration,
+    )
+
+from cript.nodes.supporting_nodes import (
+    File,
+    Group,
+    User,
+    )

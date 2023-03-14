b4af8b4001cc88490516a3d58965a5c3e03be881
nh916 <navidhariri25@gmail.com>
2023-03-14 13:49:07 -0700

    added error for InvalidVocabulary

--- src/cript/api/exceptions.py
+++ src/cript/api/exceptions.py
@@ -6,5 +6,26 @@ class CRIPTConnectionError(Exception):
     def __init__(self):
         pass
 
-    def __str__(self):
+    def __str__(self) -> str:
+        """
+
+        Returns
+        -------
+        str
+            Explanation of the error
+        """
         return "Could not connect to CRIPT with the given host and token. Please be sure both host and token are entered correctly."
+
+
+class InvalidVocabulary(Exception):
+    """
+    Raised when the CRIPT controlled vocabulary is invalid
+    """
+    # TODO add a the correct URL here
+    vocab_URL: str = "https://cript.org/controlled-vocabulary"
+
+    def __init__(self):
+        pass
+
+    def __str__(self) -> str:
+        return f"The vocabulary entered does not exist within the CRIPT controlled vocabulary. Please pick a valid CRIPT vocabulary from {self.vocab_URL}"

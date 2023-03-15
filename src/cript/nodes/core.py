10d057157d9ecaa52aefff5c6dce1f1532992f5f
Ludwig Schneider <ludwigschneider@uchicago.edu>
2023-03-15 14:13:52 -0500

    implement removing nodes fully

--- src/cript/nodes/core.py
+++ src/cript/nodes/core.py
@@ -72,3 +72,43 @@ class BaseNode(ABC):
         from cript.nodes.util import NodeEncoder
 
         return json.dumps(self, cls=NodeEncoder)
+
+    def remove_child(self, child:BaseNode) -> bool:
+        """
+        This safely removes the first found child node from the parent.
+        This requires exact node as we test with `is` instead of `==`.
+
+        returns True if child was found and deleted, False if child not found, raise DB schema exception if deletion violates DB schema.
+        """
+
+        # If we delete a child, we have to replace that with a default value.
+        # The easiest way to access this default value is to get it from the the default JsonAttribute of that class
+        default_json_attrs = self.JsonAttributes()
+        new_attrs = self._json_attrs
+        for field in self._json_attrs.__dataclass_fields__:
+            value = getattr(self._json_attrs, field)
+            if value is child:
+                new_attrs = replace(new_attrs, **{field: getattr(default_json_attrs, field)})
+                # We only want to delete the first found child
+            elif not isinstance(value, str): # Strings are iterable, but we don't want them
+                try:# Try if we are facing a list at the moment
+                    new_attr_list = [element for element in value]
+                except TypeError:
+                    pass # It is OK if this field is not a list
+                else:
+                    found_child = False
+                    for i, list_value in enumerate(value):
+                        if list_value is child:
+                            found_child = True
+                            del new_attr_list[i]
+                            # Only delete first child.
+                            # Important to break loop here, since value and new_attr_list are not identical any more.
+                    if found_child:
+                        new_attrs = replace(new_attrs, **{field: new_attr_list})
+                        # Again only first found place is removed
+                        break
+        # Let's see if we found the child aka the new_attrs are different than the old ones
+        if new_attrs is self._json_attrs:
+            return False
+        self._update_json_attrs_if_valid(new_attrs)
+        return True

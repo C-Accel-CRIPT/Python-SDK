b14365ada4edbe53dd6c69e87b93d392ca119ea5
nh916 <navidhariri25@gmail.com>
2023-03-15 18:07:39 -0700

    added API to __init__.py

--- src/cript/__init__.py
+++ src/cript/__init__.py
@@ -1,3 +1,7 @@
+from cript.api.api import API
+
+from cript.api.vocabulary import is_vocab_valid
+
 from cript.nodes import (
     Collection,
     ComputationalProcess,
---
+++ src/cript/api/__init__.py
@@ -0,0 +1,2 @@
+from cript.api.api import API
+from cript.api.vocabulary import is_vocab_valid

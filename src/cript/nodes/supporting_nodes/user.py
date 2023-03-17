09b48b3142e8f81ae0d415efd066502d5220509f
nh916 <navidhariri25@gmail.com>
2023-03-16 18:03:59 -0700

    added user node and tests

--- src/cript/nodes/supporting_nodes/user.py
+++ src/cript/nodes/supporting_nodes/user.py
@@ -1,11 +1,12 @@
 from dataclasses import dataclass
-from typing import List, Union
+from typing import List, Union, Any
 
 from cript.nodes.core import BaseNode
 from cript.nodes.supporting_nodes.group import Group
+from cript.nodes.exceptions import UneditableAttributeError
 
 
-class User:
+class User(BaseNode):
     """
     The User node
 
@@ -28,27 +29,136 @@ class User:
     _json_attrs: JsonAttributes = JsonAttributes()
 
     def __init__(
-        self, username: str, email: str, orcid: str, groups: Union[Group, List[Group]]
+        self,
+        username: str,
+        email: str,
+        orcid: str,
+        groups: Union[Group, List[Group]] = None,
     ):
         """
         Json from CRIPT API to be converted to a node
+        optionally the group can be None if the user doesn't have a group
 
         Parameters
         ----------
-        json
+        username
+        email
+        orcid
+        groups
         """
+        super().__init__(node="User")
         pass
 
     # ------------------ properties ------------------
 
     @property
     def username(self) -> str:
+        """
+        username of the User node
+
+        Returns
+        -------
+        str
+            username of the User node
+        """
         return self._json_attrs.username
+
+    @username.setter
+    def username(self, value: Any) -> None:
+        """
+        User cannot set the username for the user node.
+        Attempt to do so will raise an UneditableAttributeError
+
+        Parameters
+        ----------
+        value
+
+        Returns
+        -------
+        None
+        """
+        raise UneditableAttributeError
 
     @property
     def email(self) -> str:
+        """
+        email of the user node
+
+        Returns
+        -------
+        str
+            User node email
+        """
         return self._json_attrs.email
+
+    @email.setter
+    def email(self, value: Any) -> None:
+        """
+        User cannot set the email for the user node.
+        Attempt to do so will raise an UneditableAttributeError
+
+        Parameters
+        ----------
+        value
+
+        Returns
+        -------
+        None
+        """
+        raise UneditableAttributeError
 
     @property
     def orcid(self) -> str:
+        """
+        users ORCID
+
+        Returns
+        -------
+        str
+            users ORCID
+        """
         return self._json_attrs.orcid
+
+    @orcid.setter
+    def orcid(self, value: Any) -> None:
+        """
+        User cannot set the orcid for the user node.
+        Attempt to do so will raise an UneditableAttributeError
+
+        Parameters
+        ----------
+        value
+
+        Returns
+        -------
+        None
+        """
+        raise UneditableAttributeError
+
+    @property
+    def groups(self):
+        """
+        gets the list of group nodes that the user belongs in
+
+        Returns
+        -------
+        List[Group]
+            List of Group nodes that the user belongs in
+        """
+        return self._json_attrs.groups
+
+    @groups.setter
+    def groups(self, value: Any):
+        """
+        User cannot set the group for the user node.
+        Attempt to do so will raise an UneditableAttributeError
+
+        Parameters
+        ----------
+        value
+
+        Returns
+        -------
+        None
+        """
+        raise UneditableAttributeError
--- tests/nodes/supporting_nodes/test_user.py
+++ tests/nodes/supporting_nodes/test_user.py
@@ -8,8 +8,64 @@ def test_create_user():
     pass
 
 
-def tes_user_to_json():
+def test_user_to_json():
     """
     tests that a user node can be converted to JSON correctly
+    """
+    pass
+
+
+def test_get_username():
+    """
+    tests that it can get the username correctly
+    """
+    pass
+
+
+def test_set_username():
+    """
+    tests that setting a username will raise an UneditableNodeError
+    """
+    pass
+
+
+def test_get_email():
+    """
+    tests that it can get the email correctly
+    """
+    pass
+
+
+def test_set_email():
+    """
+    tests that setting a email will raise an UneditableNodeError
+    """
+    pass
+
+
+def test_get_orcid():
+    """
+    tests that it can get the orcid correctly
+    """
+    pass
+
+
+def test_set_orcid():
+    """
+    tests that setting a orcid will raise an UneditableNodeError
+    """
+    pass
+
+
+def test_get_user_group():
+    """
+    tests that it can correctly get a group node from the user node
+    """
+    pass
+
+
+def test_set_user_group():
+    """
+    tests that setting the user attribute raises an UneditableNodeError
     """
     pass

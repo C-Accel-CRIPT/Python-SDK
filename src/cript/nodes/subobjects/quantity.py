26d8e7b040d684ee636c628edbee162a5688460d
Ludwig Schneider <ludwigschneider@uchicago.edu>
2023-03-21 17:29:34 -0500

    Add Quantity implementation

--- src/cript/nodes/subobjects/quantity.py
+++ src/cript/nodes/subobjects/quantity.py
@@ -1,6 +1,72 @@
-class Quantity:
+from dataclasses import dataclass, replace
+from numbers import Number
+from typing import Union
+
+from cript.nodes.core import BaseNode
+
+
+class Quantity(BaseNode):
     """
     Quantity subobject
     """
 
-    pass
+    @dataclass(frozen=True)
+    class JsonAttributes(BaseNode.JsonAttributes):
+        node: str = "Quantity"
+        key: str = ""
+        value: Union[Number, None] = None
+        unit: str = ""
+        uncertainty: Union[Number, None] = None
+        uncertainty_type: str = ""
+
+    _json_attrs: JsonAttributes = JsonAttributes()
+
+    def __init__(
+        self, key: str, value: Number, unit: str, uncertainty: Union[Number, None] = None, uncertainty_type: str = "", **kwargs
+    ):
+        super().__init__(node="Quantity")
+        self._json_attrs = replace(
+            self._json_attrs, key=key, value=value, unit=unit, uncertainty=uncertainty, uncertainty_type=uncertainty_type
+        )
+        self.validate()
+
+    @property
+    def key(self) -> str:
+        return self._json_attrs.key
+
+    @key.setter
+    def key(self, new_key: str):
+        new_attrs = replace(self._json_attrs, key=new_key)
+        self._update_json_attrs_if_valid(new_attrs)
+
+    @property
+    def value(self) -> Union[int, float, str]:
+        return self._json_attrs.value
+
+    @value.setter
+    def value(self, new_value: Union[int, float, str]):
+        new_attrs = replace(self._json_attrs, value=new_value)
+        self._update_json_attrs_if_valid(new_attrs)
+
+    @property
+    def unit(self) -> str:
+        return self._json_attrs.unit
+
+    @unit.setter
+    def unit(self, new_unit: str):
+        new_attrs = replace(self._json_attrs, unit=new_unit)
+        self._update_json_attrs_if_valid(new_attrs)
+
+    @property
+    def uncertainty(self):
+        return self._json_attrs.uncertainty
+
+    @property
+    def uncertainty_type(self):
+        return self._json_attrs.uncertainty_type
+
+    # It only makes sense to set uncertainty and uncertainty type at the same time.
+    # So no individual setters, just a combination
+    def set_uncertainty(self, uncertainty: Number, type: str):
+        new_attrs = replace(self._json_attrs, uncertainty=uncertainty, uncertainty_type=type)
+        self._update_json_attrs_if_valid(new_attrs)

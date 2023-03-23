870641d2e1821c5844d0c8b7e52c4bb009279693
nh916 <navidhariri25@gmail.com>
2023-03-22 17:31:20 -0700

    setup computation.py node

--- src/cript/nodes/primary_nodes/computation.py
+++ src/cript/nodes/primary_nodes/computation.py
@@ -1,9 +1,231 @@
+from dataclasses import dataclass, replace
+from typing import List
+
+from cript import Data, SoftwareConfiguration, Condition, Citation
+from cript.nodes.core import BaseNode
 from cript.nodes.primary_nodes.primary_base_node import PrimaryBaseNode
 
 
 class Computation(PrimaryBaseNode):
     """
     Computation node
+
+    [Computation Node](https://pubs.acs.org/doi/suppl/10.1021/acscentsci.3c00011/suppl_file/oc3c00011_si_001.pdf#page=14)
     """
 
-    pass
+    @dataclass(frozen=True)
+    class JsonAttributes(BaseNode.JsonAttributes):
+        """
+        all computation nodes attributes
+        """
+
+        node: str = "Computation"
+        type: str = ""
+        input_data: List[Data] = None
+        output_data: List[Data] = None
+        software_configurations: List[SoftwareConfiguration] = None
+        conditions: List[Condition] = None
+
+        # TODO attribute is the class
+        #   this needs getters and setters and to be included in the constructor
+        # prerequisite_computation: Computation = None
+
+        citations: List[Citation] = None
+
+    def __init__(
+        self,
+        type: str,
+        input_data: List[Data] = None,
+        output_data: List[Data] = None,
+        software_configurations: List[SoftwareConfiguration] = None,
+        conditions: List[Condition] = None,
+        citations: List[Citation] = None,
+    ) -> None:
+        super().__init__(node="Computation")
+
+        self._json_attrs = replace(
+            self._json_attrs,
+            type=type,
+            input_data=input_data,
+            output_data=output_data,
+            software_configurations=software_configurations,
+            conditions=conditions,
+            citations=citations,
+        )
+
+        self.validate()
+
+    # --------- Properties ---------
+
+    @property
+    def type(self) -> str:
+        """
+        get the computation type
+
+        the computation type must come from CRIPT controlled vocabulary
+
+        Returns
+        -------
+        str
+        """
+        return self._json_attrs.type
+
+    @type.setter
+    def type(self, new_computation_type: str) -> None:
+        """
+        set the computation type
+
+        the computation type must come from CRIPT controlled vocabulary
+
+        Parameters
+        ----------
+        new_computation_type: str
+
+        Returns
+        -------
+        None
+        """
+        new_attrs = replace(self._json_attrs, type=new_computation_type)
+        self._update_json_attrs_if_valid(new_attrs)
+
+    @property
+    def input_data(self) -> List[Data]:
+        """
+        get the list of input data (data nodes) for this node
+
+        Returns
+        -------
+        List[Data]
+        """
+        return self._json_attrs.input_data
+
+    @input_data.setter
+    def input_data(self, new_input_data_list: List[Data]) -> None:
+        """
+        set the input data list
+
+        Parameters
+        ----------
+        new_input_data_list: List[Data]
+            list of input data (data nodes) to replace the current
+
+        Returns
+        -------
+        None
+        """
+        new_attrs = replace(self._json_attrs, input_data=new_input_data_list)
+        self._update_json_attrs_if_valid(new_attrs)
+
+    @property
+    def output_data(self) -> List[Data]:
+        """
+        get the list of output data (data nodes)
+
+        Returns
+        -------
+        List[Data]
+        """
+        return self._json_attrs.output_data
+
+    @output_data.setter
+    def output_data(self, new_output_data_list: List[Data]) -> None:
+        """
+        set the list of output data (data nodes) for this node
+
+        Parameters
+        ----------
+        new_output_data_list: List[Data]
+            replace the current list of output data for this node
+
+        Returns
+        -------
+        None
+        """
+        new_attrs = replace(self._json_attrs, output_data=new_output_data_list)
+        self._update_json_attrs_if_valid(new_attrs)
+
+    @property
+    def software_configurations(self) -> List[SoftwareConfiguration]:
+        """
+        get the software_configurations for this computation node
+
+        Returns
+        -------
+        List[SoftwareConfiguration]
+        """
+        return self._json_attrs.software_configurations
+
+    @software_configurations.setter
+    def software_configurations(
+        self, new_software_configurations_list: List[SoftwareConfiguration]
+    ) -> None:
+        """
+        set the list of software_configurations for this computation node
+
+        Parameters
+        ----------
+        new_software_configurations_list: List[software_configurations]
+            new_software_configurations_list to replace the current one
+
+        Returns
+        -------
+        None
+        """
+        new_attrs = replace(
+            self._json_attrs, software_configurations=new_software_configurations_list
+        )
+        self._update_json_attrs_if_valid(new_attrs)
+
+    @property
+    def conditions(self) -> List[Condition]:
+        """
+        get the list of conditions for this computation node
+
+        Returns
+        -------
+        List[Condition]
+        """
+        return self._json_attrs.conditions
+
+    @conditions.setter
+    def conditions(self, new_condition_list: List[Condition]) -> None:
+        """
+        set the list of conditions for this node
+
+        Parameters
+        ----------
+        new_condition_list: List[Condition]
+
+        Returns
+        -------
+        None
+        """
+        new_attrs = replace(self._json_attrs, conditions=new_condition_list)
+        self._update_json_attrs_if_valid(new_attrs)
+
+    @property
+    def citations(self) -> List[Citation]:
+        """
+        get the list of citations for this computation node
+
+        Returns
+        -------
+        List[Citation]
+        """
+        return self._json_attrs.citations
+
+    @citations.setter
+    def citations(self, new_citations_list: List[Citation]) -> None:
+        """
+        set the list of citations
+
+        Parameters
+        ----------
+        new_citations_list: List[Citation]
+
+        Returns
+        -------
+        None
+        """
+        new_attrs = replace(self._json_attrs, citations=new_citations_list)
+        self._update_json_attrs_if_valid(new_attrs)

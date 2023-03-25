db8811414af818253ccd5dddcb5444d0b5960a40
nh916 <navidhariri25@gmail.com>
2023-03-24 17:59:45 -0700

    updated test_collection.py

--- tests/nodes/primary_nodes/test_collection.py
+++ tests/nodes/primary_nodes/test_collection.py
@@ -1,17 +1,7 @@
-import cript
 import pytest
 
-
-def test_create_collection_slim() -> None:
-    """
-    test just to see if a collection can be made without any issues
-    with just a collection name and nothing else
-
-    Returns
-    -------
-    None
-    """
-    my_collection = cript.Collection(name="my collection name")
+import cript
+from cript import Collection
 
 
 def test_create_collection_full() -> None:
@@ -23,3 +13,20 @@ def test_create_collection_full() -> None:
     None
     """
     my_collection = cript.Collection(name="my collection name")
+
+
+@pytest.fixture(scope="session")
+def collection_object() -> Collection:
+    """
+    test just to see if a collection can be made without any issues
+    with just a collection name and nothing else
+
+    Notes
+    -----
+    this object is later used for other test
+
+    Returns
+    -------
+    Collection
+    """
+    pass

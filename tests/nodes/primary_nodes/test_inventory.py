c5d32eea0cf15ce312ecf26d62e6a7472d6b21dc
nh916 <navidhariri25@gmail.com>
2023-03-29 15:03:18 -0700

    test_create_inventory successful!
    * changed inventory material to `materials` to make it more self-documenting

--- tests/nodes/primary_nodes/test_inventory.py
+++ tests/nodes/primary_nodes/test_inventory.py
@@ -5,11 +5,27 @@ import pytest
 def test_create_inventory() -> None:
     """
     tests that an inventory node can be successfully created without errors
-
-    Returns
-    -------
-    None
     """
+    material_1 = cript.Material(name="material 1", identifiers=[{"alternative_names": "material 1 alternative name"}])
+
+    material_2 = cript.Material(name="material 2", identifiers=[{"alternative_names": "material 2 alternative name"}])
+
+    my_inventory = cript.Inventory(materials_list=[material_1, material_2])
+
+    assert isinstance(my_inventory, cript.Inventory)
+    assert my_inventory.materials == [material_1, material_2]
+
+
+@pytest.fixture(scope="session")
+def inventory_node():
+    """
+    create inventory node for other tests to use
+    """
+    # create an inventory node
+
+    # use inventory node
+
+    # reset inventory node to original state
     pass
 
 
@@ -32,7 +48,7 @@ def test_get_and_set_inventory() -> None:
 # --------------- Integration Tests ---------------
 
 
-def test_save_inventory() -> None:
+def test_save_inventory(cript_api) -> None:
     """
     test that an inventory node can be saved correctly to the API
 
@@ -52,7 +68,7 @@ def test_save_inventory() -> None:
     pass
 
 
-def test_update_inventory() -> None:
+def test_update_inventory(cript_api) -> None:
     """
     test an inventory node can be correctly updated in the API
 
@@ -67,7 +83,7 @@ def test_update_inventory() -> None:
     pass
 
 
-def test_delete_inventory() -> None:
+def test_delete_inventory(cript_api) -> None:
     """
     simply test that an inventory node can be correctly deleted from the API
 

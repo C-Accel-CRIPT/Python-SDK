df165b73a8ffa727e3804e7643a772e5ca301c68
nh916 <navidhariri25@gmail.com>
2023-03-29 13:38:22 -0700

    successfully running simple process test

--- tests/nodes/primary_nodes/test_process.py
+++ tests/nodes/primary_nodes/test_process.py
@@ -1,2 +1,22 @@
 import cript
 import pytest
+
+
+def test_simple_process() -> None:
+    """
+    tests that a simple process node can be correctly created
+    """
+
+    # process fields
+    my_process_type = "affinity_pure"
+    my_process_description = "my simple material description"
+    my_process_keywords = ["anionic"]
+
+    # create process node
+    my_process = cript.Process(type=my_process_type, description=my_process_description, keywords=my_process_keywords)
+
+    # assertions
+    assert isinstance(my_process, cript.Process)
+    assert my_process.type == my_process_type
+    assert my_process.description == my_process_description
+    assert my_process.keywords == my_process_keywords

722f6dc71f6788d8fef62014e01ec355dafc7fcf
nh916 <navidhariri25@gmail.com>
2023-03-31 16:44:50 -0700

    formatted test_computational_process.py

--- tests/nodes/primary_nodes/test_computational_process.py
+++ tests/nodes/primary_nodes/test_computational_process.py
@@ -18,13 +18,13 @@ def test_create_simple_computational_process(simple_data_node, simple_ingredient
 
 
 def test_create_complex_computational_process(
-        simple_data_node,
-        simple_material_node,
-        simple_ingredient_node,
-        simple_software_configuration,
-        simple_condition_node,
-        simple_property_node,
-        simple_citation_node,
+    simple_data_node,
+    simple_material_node,
+    simple_ingredient_node,
+    simple_software_configuration,
+    simple_condition_node,
+    simple_property_node,
+    simple_citation_node,
 ) -> None:
     """
     create a complex computational process with all possible arguments

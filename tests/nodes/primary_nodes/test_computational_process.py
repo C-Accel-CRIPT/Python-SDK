5a15abf4b54a7b4e200bce6bcef36f2d23675391
nh916 <navidhariri25@gmail.com>
2023-03-31 16:36:56 -0700

    added Integration tests to test_computational_process.py

--- tests/nodes/primary_nodes/test_computational_process.py
+++ tests/nodes/primary_nodes/test_computational_process.py
@@ -18,13 +18,13 @@ def test_create_simple_computational_process(simple_data_node, simple_ingredient
 
 
 def test_create_complex_computational_process(
-    simple_data_node,
-    simple_material_node,
-    simple_ingredient_node,
-    simple_software_configuration,
-    simple_condition_node,
-    simple_property_node,
-    simple_citation_node,
+        simple_data_node,
+        simple_material_node,
+        simple_ingredient_node,
+        simple_software_configuration,
+        simple_condition_node,
+        simple_property_node,
+        simple_citation_node,
 ) -> None:
     """
     create a complex computational process with all possible arguments
@@ -55,4 +55,38 @@ def test_create_complex_computational_process(
     assert my_computational_process.citations == [simple_citation_node]
 
 
-# TODO add integration tests
+# ---------- Integration tests ----------
+def test_save_computational_process_to_api() -> None:
+    """
+    tests if the computational_process node can be saved to the API without errors and status code of 200
+    """
+    pass
+
+
+def test_get_computational_process_from_api() -> None:
+    """
+    integration test: gets the computational_process node from the api that was saved prior
+    """
+    pass
+
+
+def test_serialize_json_to_computational_process() -> None:
+    """
+    tests that a JSON of a computational_process node can be correctly converted to python object
+    """
+    pass
+
+
+def test_update_computational_process_in_api() -> None:
+    """
+    tests that the computational_process node can be correctly updated within the API
+    """
+    pass
+
+
+def test_delete_computational_process_from_api() -> None:
+    """
+    integration test: tests that the computational_process node can be deleted correctly from the API
+    tries to get the computational_process from API, and it is expected for the API to give an error response
+    """
+    pass

eb9e25ace49801e8739dcdc042915588e1b0a868
nh916 <navidhariri25@gmail.com>
2023-03-30 19:01:25 -0700

    test_create_simple_computational_process successful

--- tests/nodes/primary_nodes/test_computational_process.py
+++ tests/nodes/primary_nodes/test_computational_process.py
@@ -3,40 +3,20 @@ import pytest
 import cript
 
 
-def test_create_simple_computational_process() -> None:
+def test_create_simple_computational_process(simple_data_node, simple_ingredient_node) -> None:
     """
     create a simple computational_process node with required arguments
     """
-    my_computational_process_type = "cross_linking"
-
-    # input data
-    my_data_files = cript.File(
-        source="https://criptapp.org", type="calibration", extension=".csv",
-        data_dictionary="my file's data dictionary"
-    )
-
-    input_data = cript.Data(type="afm_amp", files=[my_data_files])
-
-    # ingredients with Material and Quantity node
-    my_material = cript.Material(name="my material",
-                                 identifiers=[{"alternative_names": "my material alternative name"}])
-
-    my_quantity = cript.Quantity(key="mass", value=1.23, unit="gram")
-
-    ingredients = cript.Ingredient(
-        material=my_material,
-        quantities=[my_quantity],
-    )
 
     my_computational_process = cript.ComputationalProcess(
-        type=my_computational_process_type, input_data=[input_data], ingredients=[ingredients]
+        type="cross_linking", input_data=[simple_data_node], ingredients=[simple_ingredient_node]
     )
 
     # assertions
     assert isinstance(my_computational_process, cript.ComputationalProcess)
-    assert my_computational_process.type == my_computational_process_type
-    assert my_computational_process.input_data == [input_data]
-    assert my_computational_process.ingredients == [ingredients]
+    assert my_computational_process.type == "cross_linking"
+    assert my_computational_process.input_data == [simple_data_node]
+    assert my_computational_process.ingredients == [simple_ingredient_node]
 
 
 def test_create_complex_computational_process() -> None:

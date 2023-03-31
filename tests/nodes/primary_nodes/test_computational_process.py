e3e676c6572565f3b5ee4ba062378e679521d788
nh916 <navidhariri25@gmail.com>
2023-03-30 18:50:38 -0700

    test_create_simple_computational_process successful!

--- tests/nodes/primary_nodes/test_computational_process.py
+++ tests/nodes/primary_nodes/test_computational_process.py
@@ -10,28 +10,72 @@ def test_create_simple_computational_process() -> None:
     my_computational_process_type = "cross_linking"
 
     # input data
-    my_data_files = [
-        cript.File(
-            source="https://criptapp.org", type="calibration", extension=".csv", data_dictionary="my file's data dictionary"
-        )
-    ]
+    my_data_files = cript.File(
+        source="https://criptapp.org", type="calibration", extension=".csv",
+        data_dictionary="my file's data dictionary"
+    )
 
-    input_data = [cript.Data(type="afm_amp", files=my_data_files)]
+    input_data = cript.Data(type="afm_amp", files=[my_data_files])
 
     # ingredients with Material and Quantity node
-    ingredients = [
-        cript.Ingredient(
-            material=cript.Material(name="my material", identifiers=[{"alternative_names": "my material alternative name"}]),
-            quantities=[cript.Quantity(key="mass", value=1.23, unit="gram")],
-        )
-    ]
+    my_material = cript.Material(name="my material",
+                                 identifiers=[{"alternative_names": "my material alternative name"}])
+
+    my_quantity = cript.Quantity(key="mass", value=1.23, unit="gram")
+
+    ingredients = cript.Ingredient(
+        material=my_material,
+        quantities=[my_quantity],
+    )
 
     my_computational_process = cript.ComputationalProcess(
-        type=my_computational_process_type, input_data=input_data, ingredients=ingredients
+        type=my_computational_process_type, input_data=[input_data], ingredients=[ingredients]
     )
 
     # assertions
     assert isinstance(my_computational_process, cript.ComputationalProcess)
     assert my_computational_process.type == my_computational_process_type
-    assert my_computational_process.input_data == input_data
-    assert my_computational_process.ingredients == ingredients
+    assert my_computational_process.input_data == [input_data]
+    assert my_computational_process.ingredients == [ingredients]
+
+
+def test_create_complex_computational_process() -> None:
+    """
+    create a complex computational process with all possible arguments
+    """
+
+    # # all computational_process attributes
+    #
+    # my_computational_process_type = "cross_linking"
+    #
+    # # input data
+    # input_data_files = cript.File(
+    #     source="https://criptapp.org", type="calibration", extension=".csv",
+    #     data_dictionary="my file's data dictionary"
+    # )
+    #
+    # input_data = cript.Data(type="afm_amp", files=[input_data_files])
+    #
+    # # output data
+    # output_data_files = cript.File(
+    #     source="https://criptscripts.org", type="calibration", extension=".csv",
+    #     data_dictionary="my file's data dictionary"
+    # )
+    #
+    # output_data = cript.Data(type="afm_amp", files=[output_data_files])
+    #
+    # # ingredients with Material and Quantity node
+    # my_material = cript.Material(name="my material",
+    #                              identifiers=[{"alternative_names": "my material alternative name"}])
+    #
+    # my_quantity = cript.Quantity(key="mass", value=1.23, unit="gram")
+    #
+    # ingredients = cript.Ingredient(
+    #     material=my_material,
+    #     quantities=[my_quantity],
+    # )
+    #
+    # # software_configuration
+    # # TODO left off at software
+    # software_config = cript.SoftwareConfiguration(software=)
+    pass

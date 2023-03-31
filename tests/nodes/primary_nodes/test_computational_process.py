a9969f5c16411a8aeaf9267d5723a358bad69cf7
nh916 <navidhariri25@gmail.com>
2023-03-30 17:23:41 -0700

    test_create_simple_computational_process successful

--- tests/nodes/primary_nodes/test_computational_process.py
+++ tests/nodes/primary_nodes/test_computational_process.py
@@ -1,3 +1,37 @@
 import pytest
 
 import cript
+
+
+def test_create_simple_computational_process() -> None:
+    """
+    create a simple computational_process node with required arguments
+    """
+    my_computational_process_type = "cross_linking"
+
+    # input data
+    my_data_files = [
+        cript.File(
+            source="https://criptapp.org", type="calibration", extension=".csv", data_dictionary="my file's data dictionary"
+        )
+    ]
+
+    input_data = [cript.Data(type="afm_amp", files=my_data_files)]
+
+    # ingredients with Material and Quantity node
+    ingredients = [
+        cript.Ingredient(
+            material=cript.Material(name="my material", identifiers=[{"alternative_names": "my material alternative name"}]),
+            quantities=[cript.Quantity(key="mass", value=1.23, unit="gram")],
+        )
+    ]
+
+    my_computational_process = cript.ComputationalProcess(
+        type=my_computational_process_type, input_data=input_data, ingredients=ingredients
+    )
+
+    # assertions
+    assert isinstance(my_computational_process, cript.ComputationalProcess)
+    assert my_computational_process.type == my_computational_process_type
+    assert my_computational_process.input_data == input_data
+    assert my_computational_process.ingredients == ingredients

f027cfcd98ffd4bed07be9b373489a1a72c23488
nh916 <navidhariri25@gmail.com>
2023-04-03 13:01:52 -0700

    updated docstrings

--- src/cript/nodes/primary_nodes/experiment.py
+++ src/cript/nodes/primary_nodes/experiment.py
@@ -7,9 +7,7 @@ from cript.nodes.primary_nodes.primary_base_node import PrimaryBaseNode
 
 class Experiment(PrimaryBaseNode):
     """
-    Experiment node
-
-    as defined in the [data model](https://pubs.acs.org/doi/suppl/10.1021/acscentsci.3c00011/suppl_file/oc3c00011_si_001.pdf#page=9)
+    [Experiment node](https://pubs.acs.org/doi/suppl/10.1021/acscentsci.3c00011/suppl_file/oc3c00011_si_001.pdf#page=9)
     """
 
     @dataclass(frozen=True)

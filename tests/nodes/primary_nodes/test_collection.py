a809d8094baffd791eca328ff29422f4ebfa7656
nh916 <navidhariri25@gmail.com>
2023-04-03 14:12:01 -0700

    test_create_simple_collection successful

--- tests/nodes/primary_nodes/test_collection.py
+++ tests/nodes/primary_nodes/test_collection.py
@@ -4,6 +4,29 @@ import cript
 from cript import Collection
 
 
+def test_create_simple_collection(simple_experiment_node) -> None:
+    """
+    test to see a simple collection node can be created with only required arguments
+
+    Notes
+    -----
+    * [Collection](https://pubs.acs.org/doi/suppl/10.1021/acscentsci.3c00011/suppl_file/oc3c00011_si_001.pdf#page=8)
+    has no required attributes.
+    * The Python SDK only requires Collections to have `name`
+        * Since it doesn't make sense to have an empty Collection I added an Experiment to the Collection as well
+    """
+    my_collection_name = "my collection name"
+
+    my_collection = cript.Collection(name=my_collection_name, experiments=[simple_experiment_node])
+
+    # assertions
+    assert isinstance(my_collection, cript.Collection)
+    assert my_collection.name == my_collection_name
+    assert my_collection.experiments == [simple_experiment_node]
+
+
+
+
 def test_create_collection_full() -> None:
     """
     test to see if Collection can be made with all the possible options filled
@@ -13,20 +36,3 @@ def test_create_collection_full() -> None:
     None
     """
     my_collection = cript.Collection(name="my collection name")
-
-
-@pytest.fixture(scope="session")
-def collection_object() -> Collection:
-    """
-    test just to see if a collection can be made without any issues
-    with just a collection name and nothing else
-
-    Notes
-    -----
-    this object is later used for other test
-
-    Returns
-    -------
-    Collection
-    """
-    pass

a2a7a1d8a6925193e8aece0bcc09e3281a700a7b
nh916 <navidhariri25@gmail.com>
2023-04-04 11:28:33 -0700

    updated docstrings for inventory.py

--- src/cript/nodes/primary_nodes/inventory.py
+++ src/cript/nodes/primary_nodes/inventory.py
@@ -7,9 +7,7 @@ from cript.nodes.primary_nodes.primary_base_node import PrimaryBaseNode
 
 class Inventory(PrimaryBaseNode):
     """
-    Inventory Node
-
-    [Inventory Node in Data Model](https://pubs.acs.org/doi/suppl/10.1021/acscentsci.3c00011/suppl_file/oc3c00011_si_001.pdf#page=9)
+    [Inventory Node](https://pubs.acs.org/doi/suppl/10.1021/acscentsci.3c00011/suppl_file/oc3c00011_si_001.pdf#page=9)
     """
 
     @dataclass(frozen=True)

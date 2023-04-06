ab84e5730d0d71089636f7f9d9df9bfc814c7c3c
nh916 <navidhariri25@gmail.com>
2023-04-05 17:25:41 -0700

    updated material documentation

--- src/cript/nodes/primary_nodes/material.py
+++ src/cript/nodes/primary_nodes/material.py
@@ -46,6 +46,24 @@ class Material(PrimaryBaseNode):
     Warnings
     -------
     Material names Must be unique within a [Project](../project)
+
+    ```json
+    {
+        "name": "my cool material",
+        "component_count": 0,
+        "computational_forcefield_count": 0,
+        "created_at": "2023-03-14T00:45:02.196297Z",
+        "identifier_count": 0,
+        "identifiers": [],
+        "model_version": "1.0.0",
+        "node": "Material",
+        "notes": "",
+        "property_count": 0,
+        "uid": "0x24a08",
+        "updated_at": "2023-03-14T00:45:02.196276Z",
+        "uuid": "403fa02c-9a84-4f9e-903c-35e535151b08",
+    }
+    ```
     """
 
     @dataclass(frozen=True)

5c811b0dabb84bef11cace53cd6bb1cd11c0204f
nh916 <navidhariri25@gmail.com>
2023-04-13 15:40:41 -0700

    formatted _valid_search_modes.py

--- src/cript/api/_valid_search_modes.py
+++ src/cript/api/_valid_search_modes.py
@@ -1,10 +1,4 @@
 """
 the types of search that are possible on the CRIPT platform
 """
-_VALID_SEARCH_MODES = (
-    "",
-    "uuid",
-    "contains_name",
-    "exact_name",
-    "uuid_children"
-)
+_VALID_SEARCH_MODES = ("", "uuid", "contains_name", "exact_name", "uuid_children")

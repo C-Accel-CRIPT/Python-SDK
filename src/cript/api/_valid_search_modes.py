d89fdd5739553b1cb8d793d31768d290d3ad51d6
nh916 <navidhariri25@gmail.com>
2023-04-13 17:17:04 -0700

    using SearchModes as enum

--- src/cript/api/_valid_search_modes.py
+++ src/cript/api/_valid_search_modes.py
@@ -3,19 +3,15 @@ the types of search that are possible on the CRIPT platform
 """
 
 from enum import Enum
-from typing import List
-
-
-# from typing import NamedTuple other option to Enum is NamedTuple
 
 
 class SearchModes(Enum):
+    """
+    KEYS are in uppercase because that is convention
+    values are in lowercase because that is easier to type and remember
+    """
     NODE_TYPE = ""
     UUID = "uuid"
     CONTAINS_NAME = "contains_name"
     EXACT_NAME = "exact_name"
     UUID_CHILDREN = "uuid_children"
-
-
-# list of valid search mode values "", "uuid", "contains_name", etc.
-_VALID_SEARCH_MODES: List[str] = [mode.value for mode in SearchModes]

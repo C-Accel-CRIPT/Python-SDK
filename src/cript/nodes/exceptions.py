6114427486af15d549d444c7eef8d765aba127b1
nh916 <navidhariri25@gmail.com>
2023-05-02 13:53:52 -0700

    added type hinting

--- src/cript/nodes/exceptions.py
+++ src/cript/nodes/exceptions.py
@@ -63,11 +63,11 @@ class CRIPTJsonDeserializationError(CRIPTException):
     attributes within the `JsonAttributes` of that specific node
     """
 
-    def __init__(self, node_type: str, json_str: str):
+    def __init__(self, node_type: str, json_str: str) -> None:
         self.node_type = node_type
         self.json_str = json_str
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"JSON deserialization failed for node type {self.node_type} with JSON str: {self.json_str}"
 
 
@@ -76,11 +76,11 @@ class CRIPTJsonNodeError(CRIPTJsonDeserializationError):
     Exception that is raised if a `node` attribute is present, but not a single itemed list.
     """
 
-    def __init__(self, node_list, json_str):
+    def __init__(self, node_list, json_str) -> None:
         self.node_list = node_list
         self.json_str = json_str
 
-    def __str__(self):
+    def __str__(self) -> str:
         error_message: str = f"Invalid JSON contains `node` attribute {self.node_list} "
         error_message += "but this is not a list with a single element. "
         error_message += " Expected is a single element list with the node name as a single string element. "
@@ -95,9 +95,9 @@ class CRIPTJsonSerializationError(CRIPTException):
 
     """
 
-    def __init__(self, node_type: str, json_dict: str):
+    def __init__(self, node_type: str, json_dict: str) -> None:
         self.node_type = node_type
         self.json_str = str(json_dict)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"JSON Serialization failed for node type {self.node_type} with JSON dict: {self.json_str}"

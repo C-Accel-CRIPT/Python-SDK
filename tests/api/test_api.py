97c00870c563465e7ae2ad645e2fa729c85ad297
Ludwig Schneider <ludwigschneider@uchicago.edu>
2023-05-09 14:37:48 -0500

    remove api db length check

--- tests/api/test_api.py
+++ tests/api/test_api.py
@@ -60,8 +60,9 @@ def test_get_db_schema_from_api(cript_api: cript.API) -> None:
     assert bool(db_schema)
     assert isinstance(db_schema, dict)
 
-    total_fields_in_db_schema = 69
-    assert len(db_schema["$defs"]) == total_fields_in_db_schema
+    # TODO this is constantly changing, so we can't check it for now.
+    # total_fields_in_db_schema = 69
+    # assert len(db_schema["$defs"]) == total_fields_in_db_schema
 
 
 def test_is_node_schema_valid(cript_api: cript.API) -> None:

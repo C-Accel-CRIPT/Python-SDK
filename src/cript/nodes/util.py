5b49dbb787ce301f54fc4771bc2de685bf9144f1
Ludwig Schneider <ludwigschneider@uchicago.edu>
2023-05-09 11:18:11 -0500

    revert json.loads instead of dict

--- src/cript/nodes/util.py
+++ src/cript/nodes/util.py
@@ -172,7 +172,7 @@ def _node_json_hook(node_str: str) -> dict:
 
     if the node field is present then continue and convert the JSON node into a Python object
     """
-    node_dict = json.loads(node_str)
+    node_dict = dict(node_str)
     try:
         node_type_list = node_dict["node"]
     except KeyError:  # Not a node, just a regular dictionary

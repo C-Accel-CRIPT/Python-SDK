55484ad0eb710fbba5326b83125b7c8116b66caf
nh916 <navidhariri25@gmail.com>
2023-05-18 13:52:11 -0700

    updated outdated error

--- src/cript/api/api.py
+++ src/cript/api/api.py
@@ -9,7 +9,7 @@ import requests
 
 from cript.api.exceptions import (
     APIError,
-    CRIPTAPIAccessError,
+    CRIPTAPIRequiredError,
     CRIPTAPISaveError,
     CRIPTConnectionError,
     InvalidHostError,
@@ -33,7 +33,7 @@ def _get_global_cached_api():
     Raises an exception if no global API object is cached yet.
     """
     if _global_cached_api is None:
-        raise CRIPTAPIAccessError
+        raise CRIPTAPIRequiredError
     return _global_cached_api
 
 

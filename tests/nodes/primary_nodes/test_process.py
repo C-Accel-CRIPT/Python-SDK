1505afb294e3e313e02019dfb49a62f13ad0be6e
nh916 <navidhariri25@gmail.com>
2023-05-25 16:23:45 -0700

    removed unneeded `Process` tests
    * `test_simple_process`

--- tests/nodes/primary_nodes/test_process.py
+++ tests/nodes/primary_nodes/test_process.py
@@ -6,26 +6,6 @@ from util import strip_uid_from_dict
 import cript
 
 
-def test_simple_process() -> None:
-    """
-    tests that a simple process node can be correctly created
-    """
-
-    # process fields
-    my_process_type = "affinity_pure"
-    my_process_description = "my simple material description"
-    my_process_keywords = ["anionic"]
-
-    # create process node
-    my_process = cript.Process(name="my process name", type=my_process_type, description=my_process_description, keyword=my_process_keywords)
-
-    # assertions
-    assert isinstance(my_process, cript.Process)
-    assert my_process.type == my_process_type
-    assert my_process.description == my_process_description
-    assert my_process.keyword == my_process_keywords
-
-
 def test_complex_process_node(complex_ingredient_node, simple_equipment_node, complex_citation_node, simple_property_node, simple_condition_node, simple_material_node, simple_process_node, complex_equipment_node, complex_condition_node) -> None:
     """
     create a process node with all possible arguments

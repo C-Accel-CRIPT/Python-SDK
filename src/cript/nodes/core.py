c5ce4eff1579dc455587097a074351438cbf6b7a
Ludwig Schneider <ludwigschneider@uchicago.edu>
2023-06-07 10:13:37 -0500

    fix issue

--- src/cript/nodes/core.py
+++ src/cript/nodes/core.py
@@ -162,7 +162,6 @@ class BaseNode(ABC):
             # Conserve newly assigned uid if uid is default (empty)
             if getattr(attrs, field) == getattr(cls.JsonAttributes(), field):
                 attrs = replace(attrs, **{str(field): getattr(node, field)})
-        print("attrs", attrs.uuid)
 
         # But here we force even usually unwritable fields to be set.
         node._update_json_attrs_if_valid(attrs)

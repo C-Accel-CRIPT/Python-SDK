ab0fba6b0bf8adf2723d114c1e4a269ba520e29b
nh916 <navidhariri25@gmail.com>
2023-06-14 10:51:05 -0700

    Update api.py (#166)
    * Update api.py
    
    changing `_BUCKET_DIRECTORY_NAME` for Python SDK
    
    * Update CONTRIBUTORS.md
    
    adding @fatjon95 to contributors
    
    * Update CONTRIBUTORS.md
    
    removing extra spaces

--- src/cript/api/api.py
+++ src/cript/api/api.py
@@ -62,7 +62,7 @@ class API:
     _IDENTITY_POOL_ID: str = "us-east-1:555e15fe-05c1-4f63-9f58-c84d8fd6dc99"
     _COGNITO_LOGIN_PROVIDER: str = "cognito-idp.us-east-1.amazonaws.com/us-east-1_VinmyZ0zW"
     _BUCKET_NAME: str = "cript-development-user-data"
-    _BUCKET_DIRECTORY_NAME: str = "user_files"
+    _BUCKET_DIRECTORY_NAME: str = "python_sdk_files"
     _s3_client: Any = None  # type: ignore
     # trunk-ignore-end(cspell)
 

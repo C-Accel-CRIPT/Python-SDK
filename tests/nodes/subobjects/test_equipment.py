4798d334bef164e0f6a6800a04a0ff17a98d4241
nh916 <navidhariri25@gmail.com>
2023-06-20 14:57:41 -0700

    wrote `test_integration_process_equipment` but it is getting `CRIPTNodeSchemaError`

--- tests/nodes/subobjects/test_equipment.py
+++ tests/nodes/subobjects/test_equipment.py
@@ -1,7 +1,10 @@
 import copy
 import json
+import uuid
 
 from util import strip_uid_from_dict
+
+from tests.test_integration import integrate_nodes_helper
 
 
 def test_json(complex_equipment_node, complex_equipment_dict):
@@ -32,3 +35,25 @@ def test_setter_getter(complex_equipment_node, complex_condition_node, complex_f
     assert len(e2.citation) == 1
     e2.citation += [cit2]
     assert e2.citation[1] == cit2
+
+
+def test_integration_process_equipment(cript_api, simple_project_node, complex_process_node, complex_equipment_node, complex_material_node):
+    """
+    integration test between Python SDK and API Client
+
+    1. POST to API
+    1. GET from API
+    1. assert JSON sent and JSON received are the same
+    """
+    simple_project_node.name = f"test_integration_process_equipment_{uuid.uuid4().hex}"
+
+    complex_process_node.equipment = [complex_equipment_node]
+
+    simple_project_node.collection[0].experiment[0].process = [complex_process_node]
+
+    # adding orphaned material node to project, to avoid `CRIPTOrphanedNodeError`
+    # TODO getting CRIPTNodeSchemaError
+    simple_project_node.material = [complex_material_node]
+
+    integrate_nodes_helper(cript_api=cript_api, project_node=simple_project_node)
+

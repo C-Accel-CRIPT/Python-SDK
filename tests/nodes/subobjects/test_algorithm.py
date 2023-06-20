b827368041170d93003f273e52de8c6c7f5f1850
nh916 <navidhariri25@gmail.com>
2023-06-20 15:46:59 -0700

    wrote `test_integration_algorithm`
    working correctly, just needs to have SDK and API JSON checked

--- tests/nodes/subobjects/test_algorithm.py
+++ tests/nodes/subobjects/test_algorithm.py
@@ -26,7 +26,7 @@ def test_json(complex_algorithm_node, complex_algorithm_dict, complex_citation_n
     assert strip_uid_from_dict(json.loads(a2.json)) == strip_uid_from_dict(a_dict)
 
 
-def test_integration_algorithm(cript_api, simple_project_node, simple_collection_node, simple_experiment_node, simple_computation_node, complex_software_configuration_node):
+def test_integration_algorithm(cript_api, simple_project_node, simple_collection_node, simple_experiment_node, simple_computation_node, complex_software_configuration_node, complex_algorithm_node):
     """
     integration test between Python SDK and API Client
 
@@ -43,5 +43,7 @@ def test_integration_algorithm(cript_api, simple_project_node, simple_collection
     simple_project_node.collection[0].experiment[0].computation = [simple_computation_node]
 
     simple_project_node.collection[0].experiment[0].computation[0].software_configuration = [complex_software_configuration_node]
+
+    simple_project_node.collection[0].experiment[0].computation[0].software_configuration[0].algorithm = [complex_algorithm_node]
 
     integrate_nodes_helper(cript_api=cript_api, project_node=simple_project_node)

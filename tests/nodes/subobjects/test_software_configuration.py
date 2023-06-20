99354e5a680f25fe47efc791f21632743e936a80
nh916 <navidhariri25@gmail.com>
2023-06-20 15:26:20 -0700

    `test_integration_software_configuration` written correctly
    just needs to check the JSON against the API and we'll know what to do for sure

--- tests/nodes/subobjects/test_software_configuration.py
+++ tests/nodes/subobjects/test_software_configuration.py
@@ -1,9 +1,11 @@
 import copy
 import json
+import uuid
 
 from util import strip_uid_from_dict
 
 import cript
+from tests.test_integration import integrate_nodes_helper
 
 
 def test_json(complex_software_configuration_node, complex_software_configuration_dict):
@@ -34,3 +36,25 @@ def test_setter_getter(complex_software_configuration_node, complex_algorithm_no
     # assert len(sc2.citation) == 1
     # sc2.citation += [cit2]
     # assert sc2.citation[1] == cit2
+
+
+def test_integration_software_configuration(cript_api, simple_project_node, simple_collection_node, simple_experiment_node, simple_computation_node):
+    """
+    integration test between Python SDK and API Client
+
+    1. POST to API
+    1. GET from API
+    1. assert JSON sent and JSON received are the same
+    """
+    simple_project_node.name = f"test_integration_material_computational_forcefield{uuid.uuid4().hex}"
+
+    simple_project_node.collection = [simple_collection_node]
+
+    simple_project_node.collection[0].experiment = [simple_experiment_node]
+
+    simple_project_node.collection[0].experiment[0].computation = [simple_computation_node]
+
+    simple_project_node.collection[0].experiment[0].computation[0] = [simple_computation_node]
+
+    integrate_nodes_helper(cript_api=cript_api, project_node=simple_project_node)
+

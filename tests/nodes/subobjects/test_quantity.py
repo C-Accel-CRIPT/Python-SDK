a70fc6addd668913ac606e7af432270519af56a0
nh916 <navidhariri25@gmail.com>
2023-06-20 14:36:33 -0700

    wrote `test_integration_quantity` but getting `CRIPTOrphanedProcessError`

--- tests/nodes/subobjects/test_quantity.py
+++ tests/nodes/subobjects/test_quantity.py
@@ -3,6 +3,7 @@ import json
 from util import strip_uid_from_dict
 
 import cript
+from tests.test_integration import integrate_nodes_helper
 
 
 def test_json(complex_quantity_node, complex_quantity_dict):
@@ -24,3 +25,30 @@ def test_getter_setter(complex_quantity_node):
     q.set_key_unit("volume", "m**3")
     assert q.key == "volume"
     assert q.unit == "m**3"
+
+
+def test_integration_quantity(cript_api, simple_project_node, simple_material_node, complex_material_node, simple_process_node, complex_ingredient_node, complex_quantity_node):
+    """
+    integration test between Python SDK and API Client
+
+    1. POST to API
+        Project with material
+            Material has ingredient sub-object
+    1. GET JSON from API
+    1. check their fields equal
+    """
+
+    # add quantity to ingredient
+    complex_ingredient_node.set_material = [simple_material_node, complex_quantity_node]
+
+    # add ingredient to process
+    simple_process_node.ingredient = [complex_ingredient_node]
+
+    # add process to experiment
+    simple_project_node.collection[0].experiment[0].process = [simple_process_node]
+
+    # TODO getting CRIPTOrphanedProcessError
+    simple_project_node.material = [simple_material_node, complex_material_node]
+
+    integrate_nodes_helper(cript_api=cript_api, project_node=simple_project_node)
+

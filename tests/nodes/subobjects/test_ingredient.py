5f69172b25be30965ec2ce14ff29f97e2a690303
nh916 <navidhariri25@gmail.com>
2023-06-20 14:28:48 -0700

    wrote `test_integration_material_ingredient` but getting `CRIPTOrphanedProcessError`

--- tests/nodes/subobjects/test_ingredient.py
+++ tests/nodes/subobjects/test_ingredient.py
@@ -3,6 +3,7 @@ import json
 from util import strip_uid_from_dict
 
 import cript
+from tests.test_integration import integrate_nodes_helper
 
 
 def test_json(complex_ingredient_node, complex_ingredient_dict):
@@ -29,3 +30,25 @@ def test_getter_setter(complex_ingredient_node, complex_quantity_node, simple_ma
 
     i2.keyword = ["monomer"]
     assert i2.keyword == ["monomer"]
+
+
+def test_integration_material_ingredient(cript_api, simple_project_node, simple_material_node, complex_material_node, simple_process_node, complex_ingredient_node):
+    """
+    integration test between Python SDK and API Client
+
+    1. POST to API
+        Project with material
+            Material has ingredient sub-object
+    1. GET JSON from API
+    1. check their fields equal
+    """
+
+    simple_process_node.ingredient = [complex_ingredient_node]
+
+    simple_project_node.collection[0].experiment[0].process = [simple_process_node]
+
+    # TODO getting CRIPTOrphanedProcessError
+    simple_project_node.material = [simple_material_node, complex_material_node]
+
+    integrate_nodes_helper(cript_api=cript_api, project_node=simple_project_node)
+

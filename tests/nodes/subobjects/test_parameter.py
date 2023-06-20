50dd1d888cfe6900d464f096289ce4c2c7d7a3f9
nh916 <navidhariri25@gmail.com>
2023-06-20 15:49:50 -0700

    wrote `test_integration_parameter`
    getting `CRIPTJsonDeserializationError`

--- tests/nodes/subobjects/test_parameter.py
+++ tests/nodes/subobjects/test_parameter.py
@@ -1,8 +1,10 @@
 import json
+import uuid
 
 from util import strip_uid_from_dict
 
 import cript
+from tests.test_integration import integrate_nodes_helper
 
 
 def test_parameter_setter_getter(complex_parameter_node):
@@ -22,3 +24,30 @@ def test_parameter_json_serialization(complex_parameter_node, complex_parameter_
     p_dict = json.loads(p2.json)
     assert strip_uid_from_dict(p_dict) == complex_parameter_dict
     assert p2.json == p.json
+
+
+def test_integration_parameter(cript_api, simple_project_node, simple_collection_node, simple_experiment_node, simple_computation_node, complex_software_configuration_node, complex_algorithm_node, complex_parameter_node):
+    """
+    integration test between Python SDK and API Client
+
+    1. POST to API
+    1. GET from API
+    1. assert JSON sent and JSON received are the same
+    """
+    simple_project_node.name = f"test_integration_parameter_{uuid.uuid4().hex}"
+
+    simple_project_node.collection = [simple_collection_node]
+
+    simple_project_node.collection[0].experiment = [simple_experiment_node]
+
+    simple_project_node.collection[0].experiment[0].computation = [simple_computation_node]
+
+    simple_project_node.collection[0].experiment[0].computation[0].software_configuration = [complex_software_configuration_node]
+
+    simple_project_node.collection[0].experiment[0].computation[0].software_configuration[0].algorithm = [complex_algorithm_node]
+
+    simple_project_node.collection[0].experiment[0].computation[0].software_configuration[0].algorithm[0].parameter = [complex_parameter_node]
+
+    # TODO getting CRIPTJsonDeserializationError
+    integrate_nodes_helper(cript_api=cript_api, project_node=simple_project_node)
+

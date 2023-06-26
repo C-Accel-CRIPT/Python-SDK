a26f3cd4abc2648cbf91ad1ac22498b1e7513562
nh916 <navidhariri25@gmail.com>
2023-06-26 18:24:11 -0400

    deepDiff catching the correct differences
    telling deepDiff to ignore `uid` field and the rest it only checks what they have in common. It seems to compare the dicts correctly.
    
    Also had to convert from JSON to Dict for doing the comparisons

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -34,6 +34,8 @@ def integrate_nodes_helper(cript_api: cript.API, project_node: cript.Project):
         * POST to API
         * GET from API
         * deserialization from API JSON to node JSON
+
+    # TODO needs more explanation of the deepDiff and how it works
     """
 
     cript_api.save(project=project_node)
@@ -48,18 +50,6 @@ def integrate_nodes_helper(cript_api: cript.API, project_node: cript.Project):
     # try to convert api JSON project to node
     my_project_from_api = cript.load_nodes_from_json(json.dumps(my_project_from_api_dict))
 
-    # print("\n\n----------------- API JSON RECEIVED -------------------------------")
-    # print(json.dumps(my_project_from_api_dict))
-    # print("------------------------------------------------------")
-
-    # print("\n\n----------------- API JSON to Python Node -------------------------------")
-    # print(my_project_from_api.json)
-    # print("------------------------------------------------------")
-    #
-    # print("\n\n-----------------JSON SENT TO API -------------------------------")
-    # print(project_node.json)
-    # print("------------------------------------------------------")
-
     # Configure keys and blocks to be ignored by deepdiff using exclude_regex_path
     exclude_regex_paths = [
         r"root(\[.*\])?\['uid'\]"
@@ -69,34 +59,4 @@ def integrate_nodes_helper(cript_api: cript.API, project_node: cript.Project):
     diff = DeepDiff(json.loads(project_node.json), json.loads(my_project_from_api.json),
                     exclude_regex_paths=exclude_regex_paths)
 
-    print("\n\n----------------- Deep Diff -------------------------------\n\n")
-    print(diff.get("values_changed", {}))
-    print("\n\n----------------- End Deep Diff -------------------------------")
-
     assert len(diff.get("values_changed", {})) == 0
-
-    # assert are_jsons_equal(json.loads(project_node.json), json.loads(my_project_from_api.json))
-
-
-def are_jsons_equal(json1, json2):
-
-    # if same types of node, then just check that they have the same UUID
-    # and if they do we can consider that they are the same
-    if json1["node"] == json2["node"]:
-        assert json1["uuid"] == json2["uuid"]
-
-        # TODO bad code, must fix the nesting
-        for key in json1.keys():
-            # in case it is an array of objects
-            if isinstance(json1[key], list) and isinstance(json2[key], list):
-                # check that inside the array there is an object
-                if isinstance(json1[key][0], dict) and isinstance(json2[key][0], dict):
-                    # TODO check that the object is a node and not something else
-                    are_jsons_equal(json1[key], json2[key])
-
-            # in case it is just an object
-            if isinstance(json1[key], dict) and isinstance(json2[key], dict):
-                are_jsons_equal(json1[key], json2[key])
-
-    return True
-

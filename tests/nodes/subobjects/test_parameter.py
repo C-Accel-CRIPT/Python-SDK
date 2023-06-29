5598646b6c535fe74555d269c87f2cf3e45b7bf8
nh916 <navidhariri25@gmail.com>
2023-06-29 16:11:18 -0400

    `test_integration_parameter` hitting deserialization error

--- tests/nodes/subobjects/test_parameter.py
+++ tests/nodes/subobjects/test_parameter.py
@@ -26,7 +26,7 @@ def test_parameter_json_serialization(complex_parameter_node, complex_parameter_
     assert p2.json == p.json
 
 
-def test_integration_parameter(cript_api, simple_project_node, simple_collection_node, simple_experiment_node, simple_computation_node, complex_software_configuration_node, complex_algorithm_node, complex_parameter_node):
+def test_integration_parameter(cript_api, simple_project_node, simple_collection_node, simple_experiment_node, simple_computation_node, simple_software_configuration, complex_algorithm_node, complex_parameter_node):
     """
     integration test between Python SDK and API Client
 
@@ -42,7 +42,7 @@ def test_integration_parameter(cript_api, simple_project_node, simple_collection
 
     simple_project_node.collection[0].experiment[0].computation = [simple_computation_node]
 
-    simple_project_node.collection[0].experiment[0].computation[0].software_configuration = [complex_software_configuration_node]
+    simple_project_node.collection[0].experiment[0].computation[0].software_configuration = [simple_software_configuration]
 
     simple_project_node.collection[0].experiment[0].computation[0].software_configuration[0].algorithm = [complex_algorithm_node]
 

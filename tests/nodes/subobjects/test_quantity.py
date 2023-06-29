ed3984bb6dfb770fc2277bb00a9fa507419b9684
nh916 <navidhariri25@gmail.com>
2023-06-29 14:32:37 -0400

    wrote `test_integration_quantity`

--- tests/nodes/subobjects/test_quantity.py
+++ tests/nodes/subobjects/test_quantity.py
@@ -1,4 +1,5 @@
 import json
+import uuid
 
 from util import strip_uid_from_dict
 
@@ -27,7 +28,8 @@ def test_getter_setter(complex_quantity_node):
     assert q.unit == "m**3"
 
 
-def test_integration_quantity(cript_api, simple_project_node, simple_material_node, complex_material_node, simple_process_node, complex_ingredient_node, complex_quantity_node):
+def test_integration_quantity(cript_api, simple_project_node, simple_collection_node, simple_experiment_node,
+                                simple_process_node, simple_ingredient_node, simple_material_node):
     """
     integration test between Python SDK and API Client
 
@@ -38,16 +40,20 @@ def test_integration_quantity(cript_api, simple_project_node, simple_material_no
     1. check their fields equal
     """
 
-    # add quantity to ingredient
-    complex_ingredient_node.set_material = [simple_material_node, complex_quantity_node]
+    simple_project_node.name = f"test_integration_quantity_{uuid.uuid4().hex}"
+
+    # assemble needed nodes
+    simple_project_node.collection = [simple_collection_node]
+
+    simple_project_node.collection[0].experiment = [simple_experiment_node]
 
     # add ingredient to process
-    simple_process_node.ingredient = [complex_ingredient_node]
+    simple_process_node.ingredient = [simple_ingredient_node]
 
-    # add process to experiment
+    # continue assembling
     simple_project_node.collection[0].experiment[0].process = [simple_process_node]
 
-    # TODO getting CRIPTOrphanedProcessError
-    simple_project_node.material = [simple_material_node, complex_material_node]
+    # add orphaned material node to project
+    simple_project_node.material = [simple_material_node]
 
     integrate_nodes_helper(cript_api=cript_api, project_node=simple_project_node)

7f69cdcc5e68c71a26a55e6013c95a5a5ce9b1ba
nh916 <navidhariri25@gmail.com>
2023-07-11 18:51:17 -0700

    wrote the update integration test for test_algorithm.py

--- tests/nodes/subobjects/test_algorithm.py
+++ tests/nodes/subobjects/test_algorithm.py
@@ -34,6 +34,7 @@ def test_integration_algorithm(cript_api, simple_project_node, simple_collection
     1. GET from API
     1. assert JSON sent and JSON received are the same
     """
+    # ========= test create =========
     simple_project_node.name = f"test_integration_software_configuration_{uuid.uuid4().hex}"
 
     simple_project_node.collection = [simple_collection_node]
@@ -45,5 +46,11 @@ def test_integration_algorithm(cript_api, simple_project_node, simple_collection
     simple_project_node.collection[0].experiment[0].computation[0].software_configuration = [simple_software_configuration]
 
     simple_project_node.collection[0].experiment[0].computation[0].software_configuration[0].algorithm = [complex_algorithm_node]
+
+    integrate_nodes_helper(cript_api=cript_api, project_node=simple_project_node)
+
+    # ========= test update =========
+    # change a simple attribute to trigger update
+    simple_project_node.collection[0].experiment[0].computation[0].software_configuration[0].algorithm[0].type = "integration"
 
     integrate_nodes_helper(cript_api=cript_api, project_node=simple_project_node)

4b12e57ff47beff4d6a1f61f08ba2058d2794c06
nh916 <navidhariri25@gmail.com>
2023-07-18 12:40:35 -0700

    added `storage_token` to docstrings of api __init__

--- src/cript/api/api.py
+++ src/cript/api/api.py
@@ -119,6 +119,8 @@ class API:
             You can find your personal token on the cript website at User > Security Settings.
             The user icon is in the top right.
             If `None` is specified, the token is inferred from the environment variable `CRIPT_TOKEN`.
+        storage_token: str
+            This token is used to upload local files to CRIPT cloud storage when needed
         config_file_path: str
             the file path to the config.json file where the token and host can be found
 

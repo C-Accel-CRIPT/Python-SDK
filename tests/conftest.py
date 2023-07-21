9844ed7073c835351d9ae2fd81dbb4f69b0066ff
nh916 <navidhariri25@gmail.com>
2023-07-21 13:28:56 -0700

    ignoring ruff formatting error

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,4 @@
+# trunk-ignore-all(ruff/F403)
 """
 This conftest file contains simple nodes (nodes with minimal required arguments)
 and complex node (nodes that have all possible arguments), to use for testing.

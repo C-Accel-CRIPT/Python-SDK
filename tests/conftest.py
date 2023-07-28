f67b3e67f57d47784fdccedc3d8cf8b03967a4bb
nh916 <navidhariri25@gmail.com>
2023-07-28 16:03:39 -0700

    switched `HAS_INTEGRATION_TESTS_ENABLED` to `False`

--- tests/conftest.py
+++ tests/conftest.py
@@ -16,7 +16,7 @@ from fixtures.supporting_nodes import *
 
 import cript
 
-HAS_INTEGRATION_TESTS_ENABLED: bool = True
+HAS_INTEGRATION_TESTS_ENABLED: bool = False
 
 
 @pytest.fixture(scope="session", autouse=True)

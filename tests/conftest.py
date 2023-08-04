02281dd42b3e140e100574dcfc3fd95e66c65435
nh916 <navidhariri25@gmail.com>
2023-08-03 19:08:52 -0700

    working on putting together a nice conftest.py that can handle multiple environments with ease

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,6 +9,7 @@ and keeping all nodes in one file makes it easier/cleaner to create tests.
 The fixtures are all functional fixtures that stay consistent between all tests.
 """
 import os
+from pathlib import Path
 
 import pytest
 from fixtures.primary_nodes import *
@@ -21,6 +22,11 @@ import cript
 # automatically gets value env vars to run integration tests
 HAS_INTEGRATION_TESTS_ENABLED: bool = os.getenv("CRIPT_TESTS").title() == "True"
 
+# indicate which CRIPT server environment you want to work with
+# puts the string in front of the config file, e.g. `tests/production_config.json`
+# TODO make this less manual with enums and functions
+CRIPT_ENVIRONMENT: str = "development".lower()
+
 
 @pytest.fixture(scope="session", autouse=True)
 def cript_api():
@@ -32,10 +38,12 @@ def cript_api():
     API: cript.API
         The created CRIPT API instance.
     """
-    storage_token = os.getenv("CRIPT_STORAGE_TOKEN")
-
     assert cript.api.api._global_cached_api is None
-    with cript.API(host=None, api_token=None, storage_token=storage_token) as api:
+
+    # config.json file path
+    config_file_path: Path = Path(__file__).parent / f"{CRIPT_ENVIRONMENT}_config.json"
+
+    with cript.API(config_file_path=config_file_path) as api:
         # using the tests folder name within our cloud storage
         api._BUCKET_DIRECTORY_NAME = "tests"
         yield api

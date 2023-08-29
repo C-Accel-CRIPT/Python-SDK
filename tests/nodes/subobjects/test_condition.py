3e58dc14dd4763f257edf26dc2e8efdabfeaf1ff
nh916 <navidhariri25@gmail.com>
2023-08-29 12:27:40 -0700

    wrote Delete integration test for condition
    passing successfully!

--- tests/nodes/subobjects/test_condition.py
+++ tests/nodes/subobjects/test_condition.py
@@ -1,7 +1,7 @@
 import json
 import uuid
 
-from integration_test_helper import integrate_nodes_helper
+from integration_test_helper import integrate_nodes_helper, delete_integration_node_helper
 from util import strip_uid_from_dict
 
 
@@ -85,3 +85,6 @@ def test_integration_process_condition(cript_api, simple_project_node, simple_co
     simple_project_node.collection[0].experiment[0].computation[0].condition[0].descriptor = "condition descriptor UPDATED"
 
     integrate_nodes_helper(cript_api=cript_api, project_node=simple_project_node)
+
+    # ========= test delete =========
+    delete_integration_node_helper(cript_api=cript_api, node_to_delete=simple_condition_node)

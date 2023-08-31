4b4ae7c005ef908397bf0f2edfc05523f96dde6f
nh916 <navidhariri25@gmail.com>
2023-08-31 14:18:46 -0700

    formatted with trunk

--- tests/nodes/primary_nodes/test_process.py
+++ tests/nodes/primary_nodes/test_process.py
@@ -2,11 +2,13 @@ import copy
 import json
 import uuid
 
-from integration_test_helper import integrate_nodes_helper
+from integration_test_helper import (
+    delete_integration_node_helper,
+    integrate_nodes_helper,
+)
 from util import strip_uid_from_dict
 
 import cript
-from integration_test_helper import delete_integration_node_helper
 
 
 def test_simple_process() -> None:

860665d8757921a46b91e4beb15aa4f4ebf9cb7b
nh916 <navidhariri25@gmail.com>
2023-10-02 13:07:14 -0700

    added code examples with doctest for parameter.py

--- src/cript/nodes/subobjects/parameter.py
+++ src/cript/nodes/subobjects/parameter.py
@@ -83,11 +83,8 @@ class Parameter(UUIDBaseNode):
 
         Examples
         --------
-        ```python
-        import cript
-
-        my_parameter = cript.Parameter("update_frequency", 1000.0, "1/second")
-        ```
+        >>> import cript
+        >>> my_parameter = cript.Parameter("update_frequency", 1000.0, "1/second")
 
         Returns
         -------
@@ -115,9 +112,9 @@ class Parameter(UUIDBaseNode):
 
         Examples
         --------
-        ```python
-        my_parameter.key = "bond_type"
-        ```
+        >>> import cript
+        >>> my_parameter = cript.Parameter("update_frequency", 1000.0, "1/second")
+        >>> my_parameter.key = "damping_time"
 
         Returns
         -------
@@ -154,9 +151,9 @@ class Parameter(UUIDBaseNode):
 
         Examples
         --------
-        ```python
-        my_parameter.value = 1
-        ```
+        >>> import cript
+        >>> my_parameter = cript.Parameter("update_frequency", 1000.0, "1/second")
+        >>> my_parameter.value = 1
 
         Returns
         -------
@@ -191,9 +188,9 @@ class Parameter(UUIDBaseNode):
 
         Examples
         --------
-        ```python
-        my_parameter.unit = "gram"
-        ```
+        >>> import cript
+        >>> my_parameter = cript.Parameter("update_frequency", 1000.0, "1/second")
+        >>> my_parameter.unit = "gram"
 
         Returns
         -------

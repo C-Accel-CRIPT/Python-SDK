e43659c32dba06b34381160677f728a0c6d2b90d
nh916 <navidhariri25@gmail.com>
2023-11-01 16:50:05 -0700

    wrote documentation to `ExactSearchModes` enum
    added code examples with doctest and relevant links to API method

--- src/cript/api/valid_search_modes.py
+++ src/cript/api/valid_search_modes.py
@@ -41,6 +41,48 @@ class SearchModes(Enum):
 
 
 class ExactSearchModes(Enum):
-    UUID = "uuid"
-    EXACT_NAME = "exact_name"
-    BIGSMILES = "bigsmiles"
+    """
+    Available exact search modes to use with the
+    [cript.API.get_node_by_exact_match() method](../api/#cript.api.api.API.get_node_by_exact_match)
+    to retrieving single nodes.
+
+    > For more details and code examples, please review
+    > [cript.API.get_node_by_exact_match() method](../api/#cript.api.api.API.get_node_by_exact_match)
+
+    Attributes
+    ----------
+    UUID : str
+        Search by unique node UUID.
+    EXACT_NAME : str
+        Search by unique exact node name.
+    BIGSMILES: str
+        Search materials by BigSmiles identifier for an exact match.
+
+    Examples
+    -------
+    >>> import cript
+    >>> # Retrieve a node by its exact UUID
+    >>> my_material_node = api.get_node_by_exact_match(
+    ...     node_type=cript.Material,
+    ...     search_mode=cript.ExactSearchModes.UUID,
+    ...     value_to_search="e1b41d34-3bf2-4cd8-9a19-6412df7e7efc"
+    ... )   # doctest: +SKIP
+
+    >>> # Retrieve a node by its exact name
+    >>> my_project_node = api.get_node_by_exact_match(
+    ...     node_type=cript.Material,
+    ...     search_mode=cript.ExactSearchModes.EXACT_NAME,
+    ...     value_to_search="Sodium polystyrene sulfonate"
+    ... )  # doctest: +SKIP
+
+    >>> # Retrieve a material by its BigSmiles identifier
+    >>> my_material_node = api.get_node_by_exact_match(
+    ...     node_type=cript.Material,
+    ...     search_mode=cript.ExactSearchModes.BIGSMILES,
+    ...     value_to_search="{[][$]CC(C)(C(=O)OCCCC)[$][]}"
+    ... )  # doctest: +SKIP
+    """
+
+    UUID: str = "uuid"
+    EXACT_NAME: str = "exact_name"
+    BIGSMILES: str = "bigsmiles"

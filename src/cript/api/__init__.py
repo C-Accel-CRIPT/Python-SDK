91c4127d05b392d61599a008b134de3576719286
Ludwig Schneider <ludwigschneider@uchicago.edu>
2023-12-14 15:45:05 -0600

    auto fixes

--- src/cript/api/__init__.py
+++ src/cript/api/__init__.py
@@ -4,4 +4,3 @@ from cript.api.api import API
 from cript.api.data_schema import DataSchema
 from cript.api.valid_search_modes import SearchModes
 from cript.api.vocabulary_categories import VocabCategories
-from cript.api.data_schema import DataSchema
